d0f3b0a537e7b221b29460f597a0589f72ba46a1
EternaLight <EternaLight98@outlook.com>
2025-03-25 03:17:20 -0500

    Mejora de apariencia

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -4,7 +4,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Getpokemons } from "../controller/GetPokemons";
 import { Pokemon } from "../models/pokemonM";
-
+import "./Listado.css"
 
 
 const Listado = () => { 
@@ -25,9 +25,9 @@ const Listado = () => {
 
 return(
     <>
-    <h1>Listado Poke Dex</h1>
+    <h1 className="efecto-espejo">Listado Poke Dex</h1>
 
-    <header>
+    <header className="barra-buscador">
         <input 
             value={query}
             placeholder="Buscar pokemon"
@@ -41,18 +41,20 @@ return(
                 <div className="row gap-3">
 
                         {filtrarpokemon?.slice(0,151).map((pokemon)=>(
-                            <Card className="mx-auto" style={{ width: '18rem' }}>
+                            <Card className="mx-auto card" style={{ width: '18rem' }}>
                                 <Card.Header><b>Tipo: </b>{pokemon.type}</Card.Header>
-                                    <Card.Img width="80" height="100" variant="top" src={pokemon.imglarge} className="d-block mx-auto w-50"/>
-                                        <Card.Body>
-                                            <Card.Title className="text-center">{pokemon.id} - {pokemon.name}</Card.Title>
-                                            <ListGroup variant="flush" className="text-center">
-                                            <ListGroup.Item> <b> Hp: </b>{pokemon.hp}</ListGroup.Item>
-                                            <ListGroup.Item> <b>Ataque: </b>{pokemon.attack}</ListGroup.Item>
-                                            <ListGroup.Item><b>Defensa: </b>{pokemon.defense}</ListGroup.Item>
-                                            <ListGroup.Item> <b>E.Ataque: </b>{pokemon.sp_atk}</ListGroup.Item>
-                                            <ListGroup.Item> <b>E.Defensa: </b>{pokemon.sp_def}</ListGroup.Item>
-                                            <ListGroup.Item> <b>Velocidad: </b>{pokemon.speed}</ListGroup.Item>
+                                    <Card.Img  variant="top" src={pokemon.imglarge} className="d-block mx-auto w-50 img-poke"/>
+                                        <Card.Body className="card-body">
+                                            <Card.Title className="text-center title-poke">{pokemon.id} - {pokemon.name}</Card.Title>
+                                            <ListGroup variant="flush" className="text-center ">
+                                            <div className="info-poke">
+                                            <ListGroup.Item className="info-poke"> <b> Hp: </b>{pokemon.hp}</ListGroup.Item>
+                                            <ListGroup.Item className="info-poke"> <b>Ataque: </b>{pokemon.attack}</ListGroup.Item>
+                                            <ListGroup.Item className="info-poke"><b>Defensa: </b>{pokemon.defense}</ListGroup.Item>
+                                            <ListGroup.Item className="info-poke"> <b>E.Ataque: </b>{pokemon.sp_atk}</ListGroup.Item>
+                                            <ListGroup.Item className="info-poke"> <b>E.Defensa: </b>{pokemon.sp_def}</ListGroup.Item>
+                                            <ListGroup.Item className="info-poke"> <b>Velocidad: </b>{pokemon.speed}</ListGroup.Item>
+                                            </div>
                                     </ListGroup>
                                 </Card.Body>
                             </Card>
